a9687acb29e1e99995a9ac015c4224544715bfe7
Amal Nair <amal.nair16@gmail.com>
2021-12-22 17:18:22 +0530

    Add files via upload
    Basic stack input and deletion using repl/linux

---
+++ BasicStack.C
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#define N 5
+int stack[N];
+int top=-1;
+int push (void)
+{
+  int x;
+  if (top==N-1)
+  {
+    printf("Overflow condition");
+  }
+  else 
+  {
+    top++;
+    printf("Enter the number to be insterted");
+    scanf("%d",&x);
+    stack[top]=x;
+  }
+}
+int pop(void)
+{ 
+  int item;
+  if (top==-1)
+  {
+    printf("Underflow condition");
+  }
+  else 
+  {
+    item=stack[top];
+    top--;
+    printf("Deleted item is : %d", item);
+
+  }
+}
+int peek (void)
+{
+  if (top==-1)
+  {
+    printf("Underflow Condition");
+  }
+  else 
+  {
+    printf("%d",&stack[top]);
+  }
+}
+int display (void)
+{
+  int i =0;
+  for(i=top;i>=0;i++)
+  {
+    printf("%d",stack[i]);
+  }
+}
+int main ()
+{
+  push();
+  pop();
+  peek();
+  display();
+  retun 0;
+}
